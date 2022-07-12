8bd0148a205115ba1da58f7091a2cab26abc5cfa
Iago Silva <iago.silva@cerusbank.com.br>
2022-07-12 09:24:59 -0300

    basic config angular and project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,34 @@
+import { EmployeeService } from 'src/app/services/employee.service';
+import { SharedModule } from './shared/shared.module';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { EmployeeModule } from './modules/employee/employee.module';
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+import { NgbActiveModal, NgbModule } from '@ng-bootstrap/ng-bootstrap';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    SharedModule,
+    EmployeeModule,
+    HttpClientModule,
+    FormsModule,
+    NgbModule
   ],
-  providers: [],
+  providers: [
+    EmployeeService,
+    NgbActiveModal
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
